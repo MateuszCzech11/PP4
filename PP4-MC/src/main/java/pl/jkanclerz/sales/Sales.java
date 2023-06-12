9288f8e1abab7f3ec14f0af439a894154c1a5832
MateuszCzech11 <czechmateusz77@gmail.com>
2023-06-12 16:46:04 +0200

    from 05.06.23

--- PP4-MC/src/main/java/pl/jkanclerz/sales/Sales.java
+++ PP4-MC/src/main/java/pl/jkanclerz/sales/Sales.java
@@ -2,56 +2,62 @@ package pl.jkanclerz.sales;
 
 import pl.jkanclerz.sales.cart.Cart;
 import pl.jkanclerz.sales.cart.CartStorage;
+import pl.jkanclerz.sales.offering.EveryNItemLineDiscountPolicy;
 import pl.jkanclerz.sales.offering.Offer;
-import pl.jkanclerz.sales.product.NoSuchProductException;
-import pl.jkanclerz.sales.product.ProductDetails;
-import pl.jkanclerz.sales.product.ProductDetailsProvider;
-import pl.jkanclerz.sales.reservation.Reservation;
+import pl.jkanclerz.sales.offering.OfferCalculator;
+import pl.jkanclerz.sales.offering.TotalDiscountPolicy;
+import pl.jkanclerz.sales.productdetails.NoSuchProductException;
+import pl.jkanclerz.sales.productdetails.ProductDetails;
+import pl.jkanclerz.sales.productdetails.ProductDetailsProvider;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public class Sales {
     private CartStorage cartStorage;
     private ProductDetailsProvider productDetailsProvider;
+    private final OfferCalculator offerCalculator;
 
-    public Sales(CartStorage cartStorage, ProductDetailsProvider productDetailsProvider) {
+    public Sales(CartStorage cartStorage, ProductDetailsProvider productDetails, OfferCalculator offerCalculator) {
         this.cartStorage = cartStorage;
-        this.productDetailsProvider = productDetailsProvider;
+        this.productDetailsProvider = productDetails;
+        this.offerCalculator = offerCalculator;
     }
 
     public void addToCart(String customerId, String productId) {
-        Cart customersCart = loadForCustomer(customerId)
+        Cart customerCart = loadCartForCustomer(customerId)
                 .orElse(Cart.empty());
 
-        ProductDetails product = getProductDetails(productId)
+        ProductDetails product = loadProductDetails(productId)
                 .orElseThrow(() -> new NoSuchProductException());
 
-        customersCart.add(product);
+        customerCart.add(product.getId());
 
-        cartStorage.save(customerId, customersCart);
+        cartStorage.addForCustomer(customerId, customerCart);
     }
 
-    private Optional<ProductDetails> getProductDetails(String productId) {
-        return productDetailsProvider.loadForProduct(productId);
+    private Optional<ProductDetails> loadProductDetails(String productId) {
+        return productDetailsProvider.load(productId);
     }
 
-    private Optional<Cart> loadForCustomer(String customerId) {
+    private Optional<Cart> loadCartForCustomer(String customerId) {
         return cartStorage.load(customerId);
     }
 
-    public Offer getCurrentOffer(String currentCustomer) {
-        offerCalculator.calculate(this.cartStorage.load(currentCustomer));
-        return new Offer();
+    public Offer getCurrentOffer(String customerId) {
+        Cart customerCart = loadCartForCustomer(customerId)
+                .orElse(Cart.empty());
+
+        Offer offer = this.offerCalculator.calculateOffer(
+                customerCart.getCartItems(),
+                new TotalDiscountPolicy(BigDecimal.valueOf(500), BigDecimal.valueOf(50)),
+                new EveryNItemLineDiscountPolicy(5)
+        );
+
+        return offer;
     }
 
-    public PaymentData acceptOffer(String customerId) {
-
-        Offer offer = getCurrentOffer(customerId);
-
-        Reservation reservation = Reservation.from(offer);
-
-        reservationStrage.save(reservation);
-
+    public void acceptOffer() {
 
     }
 }
